7c00f49de65ea313cd4676052790c29d13b04d0f
Alexander Balagurov <oleksandr.balahurov@agilie.com>
2024-02-03 19:38:06 +0200

    add xcode project

---
+++ AudioPlayer/AudioPlayer/AudioPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioPlayerApp.swift
+//  AudioPlayer
+//
+//  Created by Alexander Balagurov on 03.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AudioPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
